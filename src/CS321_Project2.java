154c6f95ce947137cb95a5ee774d30fe69083e01
Tristan <109396050+lastCoyotes@users.noreply.github.com>
2025-10-23 12:47:19 -0700

    Create CS321_Project2.java

---
+++ src/CS321_Project2.java
@@ -0,0 +1,18 @@
+package cs321_Project2;
+
+/**
+ * The main file for the program 
+ * @author Tristan Boler
+ * @version 1.1
+ * @since 2020-03-09
+ */
+public class CS321_Project2 {
+	public static void main(String[] args) {
+		System.out.println("Lodaing Menu...");
+		
+		Menu menu = new Menu();
+		menu.RunTournament();
+		
+		System.out.println("Goodbye!");
+	}
+}
